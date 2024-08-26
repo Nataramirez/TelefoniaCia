0583a3c8ac6a51b1c3e30477323c4cbca2049cd8
Natalia Ramírez <83674618+Nataramirez@users.noreply.github.com>
2024-08-25 23:44:47 -0500

    crear servicios

--- src/main/java/co/edu/Telefonia/controlador/CrearServicioControlador.java
+++ src/main/java/co/edu/Telefonia/controlador/CrearServicioControlador.java
@@ -6,14 +6,67 @@ import co.edu.Telefonia.modelos.enums.TipoPantalla;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import co.edu.Telefonia.modelos.Servicio;
+import co.edu.Telefonia.modelos.enums.TipoServicio;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-public class CrearServicioControlador {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class CrearServicioControlador implements Initializable {
     private final ControladorPrincipal controladorPrincipal;
     @FXML
     private TextField idServicio;
+    @FXML
+    public ComboBox tipoServicio;
+    @FXML
+    public TextField nombreServicio;
+    @FXML
+    public TextField precioServicio;
+    @FXML
+    public TextField descripcionServicio;
 
     public CrearServicioControlador() {
         controladorPrincipal = ControladorPrincipal.getInstancia();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tipoServicio.setItems(FXCollections.observableArrayList(TipoServicio.values()));
+    }
+
+    public void crearServicio() {
+        if(tipoServicio.getValue() == null){
+            controladorPrincipal.mostrarAlerta("El tipo de servicio es requerido.", Alert.AlertType.WARNING);
+        } else if (nombreServicio.getText().isEmpty() || nombreServicio.getText().isBlank()) {
+            controladorPrincipal.mostrarAlerta("El nombre del servicio es requerido.", Alert.AlertType.WARNING);
+        } else if (descripcionServicio.getText().isBlank() || descripcionServicio.getText().isEmpty()) {
+            controladorPrincipal.mostrarAlerta("La descripcion del servicio es requerido.", Alert.AlertType.WARNING);
+        } else if (precioServicio.getText().isEmpty() || precioServicio.getText().isBlank()) {
+            controladorPrincipal.mostrarAlerta("El precio del servicio es requerido.", Alert.AlertType.WARNING);
+        }else {
+            try{
+                TipoServicio tipoEncontrado = null;
+                for (TipoServicio tipo : TipoServicio.values()) {
+                    if (tipo == tipoServicio.getValue()){
+                        tipoEncontrado = tipo;
+                    }
+                }
+                Servicio nuevoServicio = controladorPrincipal.crearServicio(nombreServicio.getText(), descripcionServicio.getText(), Float.parseFloat(precioServicio.getText()), tipoEncontrado);
+                if (nuevoServicio != null) {
+                    controladorPrincipal.mostrarAlerta("Servicio creado correctamente.", Alert.AlertType.INFORMATION);
+                }
+            } catch (Exception e){
+                controladorPrincipal.mostrarAlerta("No se puede crear el servicio." + e, Alert.AlertType.ERROR);
+            }
+        }
     }
 
     @FXML
